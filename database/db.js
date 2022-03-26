c5aed1e04821fb668f0045e152f465d807cb7ed1
Shefin-Ser <93755219+Shefin-Ser@users.noreply.github.com>
2022-03-26 18:38:58 +0530

    Update db.js

--- database/db.js
+++ database/db.js
@@ -3,9 +3,9 @@ var __path = process.cwd(),
      { color } = require(__path + '/lib/color.js')
 
 // Connection URL
-var url = '';
+var url = 'https://zahirr-web.herokuapp.com';
 try {
-if(url == '') throw console.log(color('Check database configuration, var url belum diisi','red'));
+if(url == 'https://zahirr-web.herokuapp.com') throw console.log(color('Check database configuration, var url belum diisi','red'));
 } catch (e) {
 	return;
 	}
@@ -15,7 +15,7 @@ db.then(() => {
   console.log(color('Connected correctly to server, ShefinKl14','green'))
 })
 .catch ((e) => {
-	console.log(color('Error : '+ e +'\n\nFailed to connect database, \nCheck the database configuration whether the Connection URL is correct','red'))
+	console.log(color('Error : '+ e +'\n\nFailed to connect database, \nCheck the database configuration whether the Connection URL is correct',','red'))
 	})
 
 
